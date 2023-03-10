dda293e4592fd8502ceb9f646a28bcc5a09a0e6b
luvor <veteratorvulp@gmail.com>
2023-03-10 10:22:06 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,54 +1,51 @@
-import './App.css';
-import Header from './components/TodoHeader/Header';
-import { useState } from 'react';
-import TodoForm from './components/TodoForm/TodoForm';
+import './App.css'
+import Header from './components/TodoHeader/Header'
+import { useState } from 'react'
+import TodoForm from './components/TodoForm/TodoForm'
 
-import Block2 from './components/Block2/block2';
-import Clock from './components/Clock/Clock';
+import Block2 from './components/Block2/block2'
+import Clock from './components/Clock/Clock'
 
 function App() {
-  const [todo,setTodo] = useState([{
-    id:1,
-    status: true,
-    name: "First list",
-    type: "work"
-  },
-  {
-    id:2,
-    status: true,
-    name: "Second list",
-    type: "work"
-  },
-  {
-    id:3,
-    status: true,
-    name: "Third list",
-    type: "work"
-  },])
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      status: true,
+      name: 'First list',
+      type: 'work',
+    },
+    {
+      id: 2,
+      status: true,
+      name: 'Second list',
+      type: 'work',
+    },
+    {
+      id: 3,
+      status: true,
+      name: 'Third list',
+      type: 'work',
+    },
+  ])
 
-  const [time, setTime] = useState("00:00:00")
-  const updateTime = (cometime) =>{
-    setTime(cometime);
+  const [time, setTime] = useState('00:00:00')
+  const updateTime = (cometime) => {
+    setTime(cometime)
     console.log(time)
   }
   return (
-    <div className='container'>
-        <Header/>
-        <section className='main'>
-          <div className='left_side'>
-          <TodoForm todo={todo} setTodo={setTodo}/>
-        
-          </div>
-          <Block2 todo={todo} setTodo={setTodo}/>
-          <div className='right_side'>
-           
-          </div>
-        </section>
-        <Clock time={time} setTime = {setTime} />
+    <div className="container">
+      <Header />
+      <section className="main">
+        <div className="left_side">
+          <TodoForm todo={todo} setTodo={setTodo} />
+        </div>
+        <Block2 todo={todo} setTodo={setTodo} />
+        <div className="right_side"></div>
+      </section>
+      <Clock time={time} setTime={setTime} />
     </div>
-      
-    
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -2,32 +2,60 @@ import React, { useState } from "react";
 import s from "./Clock.module.css";
 const Clock = (props) => {
   const [opt, setOpt] = useState();
-
+  const [disabled, setDisabled] = useState(false);
+  const [interval, setIntervalItem] = useState();
   const clockCycle = (dayTime) => {
-    console.log("inClockCycle");
-    let hr = parseInt(dayTime.slice(0, 2));
-    console.log(props.time);
+    console.log("inClockCycle", dayTime);
+    if(!dayTime) return;
+    if(dayTime === "00:00:00") return;
 
-    let mn = parseInt(dayTime.slice(3, 2));
-    let sc = parseInt(dayTime.slice(6, 2));
-    if (hr > 0) {
-      console.log(hr);
-      if (mn == -1) {
-        mn = 59;
-        hr = hr - 1;
-      }
-      if (sc == 60) {
-        mn = mn - 1;
-        sc = 0;
-      }
-      sc++;
-      let time = hr + ":" + mn + ":" + sc;
+    let hr = parseInt(dayTime.split(":")[0]);
+    let mn = parseInt(dayTime.split(":")[1]);
+    let sc = parseInt(dayTime.split(":")[2]);
 
-      props.setTime(time);
-      console.log(props.time);
-    }
+    setIntervalItem(setInterval(() => {
+      if (hr > 0) {
+        console.log("inIf", hr, mn, sc);
+        if (mn === -1) {
+          mn = 59;
+          hr--;
+        }
+        if (sc === 60) {
+          mn++;
+          sc = 0;
+        }
+        if (sc === 0) {
+          mn--;
+          sc = 59;
+        } else {
+          sc--;
+        }
+        if (mn === -1) {
+          hr--;
+          mn = 59;
+        }
+        let time = hr + ":" + mn + ":" + sc;
+        if (!disabled) {
+          props.setTime(time);
+        }
+      }
+    }, 1000))
   };
+
+  const toggleForm = () => {
+    interval && clearInterval(interval);
+    setDisabled(disabled);
+    props.setTime("00:00:00");
+  };
+  // const resetTime = (timerId) => {
+  //   props.setTime("00:00:00");
+  //   setDisabled(true);
+  //   clearInterval(timerId);
+  // }
   const setTimeFromOpt = () => {
+    interval && clearInterval(interval);
+    props.setTime("00:00:00");
+
     console.log("inFromOpt");
     let dayTime;
     switch (opt) {
@@ -68,16 +96,19 @@ const Clock = (props) => {
       <div className={s.circle}>
         <div className={s.time}>{props.time}</div>
       </div>
-      <select onChange={(e) => setOpt(e.target.value)}>
-        <option value="1 Day">1 Day</option>
-        <option value="2 Day">2 Day</option>
-        <option value="3 Day">3 Day</option>
-        <option value="4 Day">4 Day</option>
-        <option value="5 Day">5 Day</option>
-        <option value="6 Day">6 Day</option>
-        <option value="7 Day">7 Day</option>
-      </select>
-      <button onClick={setTimeFromOpt}>Go!</button>
+      <form className={`${s.form} ${disabled ? `${s.hide}` : ""}`} action="#">
+        <select onChange={(e) => setOpt(e.target.value)}>
+          <option value="1 Day">1 Day</option>
+          <option value="2 Day">2 Day</option>
+          <option value="3 Day">3 Day</option>
+          <option value="4 Day">4 Day</option>
+          <option value="5 Day">5 Day</option>
+          <option value="6 Day">6 Day</option>
+          <option value="7 Day">7 Day</option>
+        </select>
+        <button onClick={setTimeFromOpt}>Go!</button>
+      </form>
+      <button onClick={toggleForm}>reset</button>
     </div>
   );
 };
